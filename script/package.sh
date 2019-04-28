1c83127685931fbee518fdd2a01c283dab9c83f9
zhouzhuopeng <zhouzhuopeng@minieye.cc>
2019-04-28 14:40:42 +0800

    update package.sh

--- script/package.sh
+++ script/package.sh
@@ -12,7 +12,10 @@ echo "copy *.node"
 cp release/linux/msgpackBinding.node release/linux/dist
 cp node/msg_fd release/linux/dist
 cp node/status2log release/linux/dist
+
 cp docs/releaseGuide.md release/linux/dist
 cp -r dbc/ release/linux/dist/dbc/
 mkdir release/linux/dist/script/
 cp script/chmod-usb.sh release/linux/dist/script/
+mkdir release/linux/dist/config/
+cp config/config.json release/linux/dist/config/
