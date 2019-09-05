e6668f22ac259bf253c7531e2773e9aba8b7d076
yuji <yujinwpu@163.com>
2019-09-05 18:09:49 +0800

    fix bug

--- parsers/parser.py
+++ parsers/parser.py
@@ -2,7 +2,7 @@ from parsers.x1 import parse_x1
 from parsers.mobileye_q3 import parse_ifv300, parse_q3
 from parsers.mobileye_q2 import parse_mobileye
 from parsers.mqb_esp import parse_mqb
-from parsers.radar import parse_esr, parse_ars, parse_hmb, parse_bosch_mrr, parse_hawkeye_lmr, parse_fusion_mrr
+from parsers.radar import parse_esr, parse_ars, parse_hmb, parse_bosch_mrr, parse_hawkeye_lmr, parse_fusion_mrr, parse_sta77
 from parsers.drtk import parse_rtk
 from parsers.mobileye_q4 import parser_mbq4
 
