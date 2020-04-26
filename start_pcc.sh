76b196b2ed347663e0895490c71e1aaef07f8927
simon.hsu <xxn599@gmail.com>
2020-04-26 14:46:27 +0800

    clean up

--- start_pcc.sh
+++ start_pcc.sh
@@ -3,4 +3,4 @@
 #nmap -T5 -sP 192.168.98.2-254 > /dev/null
 #cat /proc/net/arp | grep 0x2 | grep 192.168.98 > ip_mac.txt
 
-python3 pcc.py config/cfg_lab.json
+python3 pcc_app.py config/cfg_lab.json
