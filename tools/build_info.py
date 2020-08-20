b61728ced90b35269d126774c015e84617fa12ac
simon.hsu <xxn599@gmail.com>
2020-08-20 20:21:13 +0800

    update pyinstaller spec for socketio app

--- tools/build_info.py
+++ tools/build_info.py
@@ -9,6 +9,6 @@ def get_git_label():
 if __name__ == "__main__":
     build_time = time.ctime()
     label = get_git_label()
-    with open('../build_info.txt', 'w') as wf:
+    with open('build_info.txt', 'w') as wf:
         wf.write(build_time+'\n')
         wf.write(label+'\n')
