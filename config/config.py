f68f67700a0aff4f43b931cfd862227daa2d826b
simon.hsu <xxn599@gmail.com>
2020-08-21 21:07:38 +0800

    add can port to cfg ver1.0

--- config/config.py
+++ config/config.py
@@ -327,8 +327,8 @@ def load_cfg(jsonspec, local='config/local.json'):
                     if item.get('params'):
                         clct.update(item['params'])
                         if clct['type'] == 'x1_collector':
-                            clct['ports']['can0']['topic'] = item['can0']
-                            clct['ports']['can1']['topic'] = item['can1']
+                            clct['ports']['can0']['topic'] = item['params']['can0']
+                            clct['ports']['can1']['topic'] = item['params']['can1']
                         if item['params'].get('is_main'):
                             main_collector = item['params']['is_main']
                 else:
